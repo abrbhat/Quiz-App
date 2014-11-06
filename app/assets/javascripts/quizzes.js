2c501e9a75c8713fda0a86605f09159a7015afd7
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2014-11-06 23:39:12 +0530

    option selector working

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	var option
 	$('#quiz-form').submit(function() { 
 	    // submit the form 
 	    $(this).ajaxSubmit({
@@ -14,4 +15,29 @@ $(document).ready(function() {
 	    // return false to prevent normal browser submit and page navigation 
 	    return false; 
 	});
+	$('.option-card').click(function(){		
+		var optionListContainer = $(this).parent().parent();
+		var options = optionListContainer.children();
+		options.each(function(option) { 
+			$(this).find('.option-selected-bar').hide(500); 
+			$(this).find('.curtain').stop().fadeTo('slow', 0.1);  
+			$(this).find('.option-selected-bar-holder').stop().fadeTo('slow', 1);  
+		});
+		$(this).find('.option-selected-bar').show();
+		$(this).find('.curtain').hide();
+		$(this).find('.option-selected-bar-holder').hide();
+		$(this).find('.option-selector-radio').prop("checked", true);
+	})
+	$('.curtain-container').hover(  
+	   function(){  
+	      $(this).find('.curtain').stop().fadeTo('slow', 0);  
+	   },  
+	   function(){ 
+	   if($(this).find('.option-selector-radio').is(':checked')) { 
+	   } 
+	   else{
+	   		//If not selected then pull curtain down on mouseout
+	      	$(this).find('.curtain').stop().fadeTo('slow', 0.1);  
+	   }	
+	});  
 });
