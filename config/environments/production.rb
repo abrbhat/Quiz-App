f9ad9b91791f7aded03377cc235d0a770b1b4f19
abrbhat <bhatnagarabhiroop@gmail.com>
2015-07-31 19:58:56 +0530

    some changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: 'photos-webapp',
-      access_key_id: 'AKIAIZ5UVV37QUAPPSUQ',
-      secret_access_key: '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'
+      bucket: ENV['AWS_S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
